a2da920c61c923562eb5ef24532999334ec10cce
xammi-1 <kmg.xammi.1@gmail.com>
2015-03-25 18:56:02 +0300

    Initial tests

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python2
 
+import sys
+import unittest
+
+from tests.MyTest import MyTestCase
+
 if __name__ == '__main__':
-    pass
+    suite = unittest.TestSuite((
+        unittest.makeSuite(MyTestCase),
+    ))
+    result = unittest.TextTestRunner().run(suite)
+    sys.exit(not result.wasSuccessful())
